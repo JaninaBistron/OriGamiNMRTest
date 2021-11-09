2fb345c40c439725d3dead5128ef15eb037776b0
JaninaBistron <56539845+JaninaBistron@users.noreply.github.com>
2021-11-09 17:06:28 +0100

    Cronbach's Alpha for all Tasks

--- calculations/2_testeval_items_reliability/01_cronbachsalpha_itemtotal.R
+++ calculations/2_testeval_items_reliability/01_cronbachsalpha_itemtotal.R
@@ -13,6 +13,12 @@
   
   # Scoring for DMc and DAc tasks (Direction Tasks with Process Data)
     vdirectiontasks_cat_c <- dplyr::select(vdirectiontasks_cat, -(1:14))
+    
+  # Scoring for all tasks (without Process Data)
+    tasks_all <- cbind(locationtasks_cat_r,vdirectiontasks_cat_r)
+      
+  # Scoring for all tasks c (with Process Data)
+    tasks_all_c <- cbind(locationtasks_cat_c,vdirectiontasks_cat_c)
 
 # Create empty list for dropped tasks  ---------------------------------------------------------------------------------
     all_dropped_tasks <- c()
@@ -148,3 +154,47 @@
           d2 <- d2+1
         }
       }
+      
+  # All tasks ---
+      
+      itemanalysis <- psych::alpha(tasks_all, check.keys=FALSE, warnings=FALSE)
+      # cronbach's alpha
+      alpha_all <- itemanalysis$total$raw_alpha
+      # item-total correlation (Trennschärfe)
+      item_corr <- itemanalysis$item.stats$r.cor
+      item_corr_all <- item_corr
+      # drop tasks
+      dropped_tasks_all <- determine_dropped_tasks(tasks_all)
+      # drop tasks by item-total correlation value
+      tasks_all_drop <- tasks_all
+      for (a in 1:8){
+        if(item_corr[[a]] <= 0.3){
+          tasks_all_drop <- dplyr::select(tasks_all, -colnames(tasks_all[a]))
+        }
+      }
+      # repeat item analysis after dropping tasks
+      itemanalysis_drop <- psych::alpha(tasks_all_drop, check.keys=FALSE, warnings=FALSE)
+      alpha_all_drop <- itemanalysis_drop$total$raw_alpha
+      
+  # All tasks c ---
+      
+      itemanalysis <- psych::alpha(tasks_all_c, check.keys=FALSE, warnings=FALSE)
+      # cronbach's alpha
+      alpha_allc <- itemanalysis$total$raw_alpha
+      # item-total correlation (Trennschärfe)
+      item_corr <- itemanalysis$item.stats$r.cor
+      item_corr_allc <- item_corr
+      # drop tasks
+      dropped_tasks_allc <- determine_dropped_tasks(tasks_all_c)
+      # drop tasks by item-total correlation value
+      tasks_allc_drop <- tasks_all_c
+      for (a in 1:8){
+        if(item_corr[[a]] <= 0.3){
+          tasks_allc_drop <- dplyr::select(tasks_all_c, -colnames(tasks_all_c[a]))
+        }
+      }
+      # repeat item analysis after dropping tasks
+      itemanalysis_drop_c <- psych::alpha(tasks_allc_drop, check.keys=FALSE, warnings=FALSE)
+      alpha_allc_drop <- itemanalysis_drop$total$raw_alpha
+      
+  
